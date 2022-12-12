28fa568e1fc61941b02a26b4fbf308fbeaaa1c24
myvicc <dir@acg.co.ua>
2022-12-12 11:33:25 +0200

    js

---
+++ index.js
@@ -0,0 +1,15 @@
+const img = document.querySelector("img");
+
+const images = [
+    "images/1.jpg",
+    "images/2.jpg",
+    "images/3.jpg",
+    "images/4.jpg",
+    "images/5.jpg",
+    "images/6.jpg",
+    "images/7.jpg",
+    "images/8.jpg",
+    "images/9.jpg",
+];
+img.src = images[Math.floor(Math.random() * 9)];
+
